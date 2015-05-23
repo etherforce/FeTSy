a6b34718ade21592f6ecbeb2d929d5cf8507a40e
Norman Jäckel <fetsy@normanjaeckel.de>
2015-05-23 16:25:21 +0200

    Added link to home on admin index page.

--- fetsy/admin.py
+++ fetsy/admin.py
@@ -1,6 +1,10 @@
 from django.contrib.admin import site
+from django.utils.translation import ugettext_lazy
 
 from .models import Tag, Ticket
+
+site.site_title = site.site_header = ugettext_lazy('FeTSy site admin')
+site.index_template = 'admin/custom-index.html'
 
 site.register(Tag)
 site.register(Ticket)
