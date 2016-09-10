e12f5817d1a4d7dda3caace19f13edefbb7f5180
Norman Jäckel <fetsy@normanjaeckel.de>
2016-09-10 13:55:17 +0200

    Added logging info to database app.

--- fetsy/database/app.py
+++ fetsy/database/app.py
@@ -23,6 +23,19 @@ class AppSession(Ticket, ApplicationSession):
         yield from super().onJoin(details)
 
 
+def get_config():
+    """
+    Returns a dictionary with the configuration of this application.
+    """
+    host = 'localhost'
+    port = '8080'
+    return {
+        'host': host,
+        'port': port,
+        'router_url': 'ws://{host}:{port}/ws'.format(host=host, port=port),
+    }
+
+
 def main():
     """
     Main entry point for the database app.
@@ -32,13 +45,21 @@ def main():
     logger = logging.getLogger(__file__)
     logger.setLevel(logging.DEBUG)
 
+    # Get application configuration
+    config = get_config()
+    webclient_url = 'http://{host}:{port}/'.format(
+        host=config['host'],
+        port=config['port'])
+
     # Setup database connection.
     client = AsyncIOMotorClient()
     database = client.fetsy
 
     # Setup and run application.
+    logger.info('Try to open webclient at {webclient_url}.'.format(
+        webclient_url=webclient_url))
     runner = ApplicationRunner(
-        url='ws://localhost:8080/ws',
+        url=config['router_url'],
         realm='realm1',
         extra={
             'database': database,
